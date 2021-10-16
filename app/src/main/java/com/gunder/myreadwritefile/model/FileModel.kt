5fdec5c8f44e0d31cfea53fa2dc9dd7d804c8e0b
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-16 16:02:49 +0700

    membuat file data class dan mengatur package aplikasi

---
+++ app/src/main/java/com/gunder/myreadwritefile/model/FileModel.kt
@@ -0,0 +1,6 @@
+package com.gunder.myreadwritefile.model
+
+data class FileModel(
+    var fileName: String? = null,
+    var data: String? = null
+)
--- app/src/main/java/com/gunder/myreadwritefile/ui/main/MainActivity.kt
+++ app/src/main/java/com/gunder/myreadwritefile/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.gunder.myreadwritefile
+package com.gunder.myreadwritefile.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gunder.myreadwritefile.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
